34884a4924f21bf35f556a3e1c92b3dd920ef5b9
gabsifuentes <gabsifuentesss@gmail.com>
2026-09-15 05:04:55 -0400

    ecelera e desacelera

--- Testes.cpp/PWM.cpp
+++ Testes.cpp/PWM.cpp
@@ -1,9 +1,24 @@
-#define PWM 9
+#define PWM 3
 
 void setup(){
     pinMode(PWM, OUTPUT);
 }
 
 void loop(){
-    
+
+    //pra acelerar o motor gradualmente
+    for(int pwm = 0; pwm <= 255; pwm++){
+        analogWrite(PWM, pwm);
+        delay(30);
+    }
+
+    delay(1000);
+
+    //desacelera gradualmente
+    for(int pwm = 0; pwm <= 255; pwm--){
+        analogWrite(PWM, pwm);
+        delay(30);
+    }
+
+    delay(1000);
 }
