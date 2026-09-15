5f4519cb096cc0dcc727e4a367e04c062225449e
Aimée <aimeecavt@proton.me>
2026-09-15 01:03:35 -0400

    teste
    mapeados os supostos pinos dos motores

--- Testes.cpp/Motor.cpp
+++ Testes.cpp/Motor.cpp
@@ -1,10 +1,22 @@
+//motor a
+#define in1 = 5;
+#define in2 = 6;
+//motor b
+#define in3 = 9;
+#define in4 = 10;
+//velocidade
+#define vel = 255;
 
-//teste de commit
 void setup(){
     Serial.begin(9600);
-    
+    //pinos motores
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
 }
 
 void loop(){
 
 }
+
