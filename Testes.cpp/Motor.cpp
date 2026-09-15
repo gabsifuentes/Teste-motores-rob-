bf4239705adec9e8f63edf6042e86dec1b78ef78
Aimée <aimeecavt@proton.me>
2026-09-15 01:31:47 -0400

    funções de movimento
    Adicionadas funções de movimento

--- Testes.cpp/Motor.cpp
+++ Testes.cpp/Motor.cpp
@@ -14,9 +14,45 @@ void setup(){
     pinMode(in2, OUTPUT);
     pinMode(in3, OUTPUT);
     pinMode(in4, OUTPUT);
+    
 }
 
 void loop(){
 
 }
 
+// Funções de movimento
+
+void motorA_h(){
+    analogWrite(in1, vel);
+    digitalWrite(in2, LOW);
+}
+
+void motorA_antihorario()){
+    digitalWrite(in1, LOW);
+    analogWrite(in2, vel);
+}
+
+void motorB_horario(){
+    analogWrite(in3, vel);
+    digitalWrite(in4, LOW);
+}
+
+void motorB_antihorario()){
+    digitalWrite(in3, LOW);
+    analogWrite(in4 vel);
+}
+
+void AB_parados(){
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+}
+
+void AB_horario(){
+    analogWrite(in1, vel);
+    digitalWrite(in2, LOW);
+    analogWrite(in3, vel);
+    digitalWrite(in4, LOW);
+}
